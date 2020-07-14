d2bd12e1c8d328b05219a63d02fe8720b13ae8ea
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-07-14 17:23:05 -0300

    Foi feito o exercicio 3 e 4

--- src/listawhile/Questao03.java
+++ src/listawhile/Questao03.java
@@ -5,10 +5,53 @@
  */
 package listawhile;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
 /**
  *
  * @author JoãoMarcus
  */
 public class Questao03 {
-    
+    public static void main(String[] args) {
+        // preço de compra e preço de venda das mesmas
+        Scanner t = new Scanner(System.in);
+        DecimalFormat d = new DecimalFormat("#0.00");
+        
+        String nameThing = "";
+        int profitTenpercent=0; int profitTwentypercent =0; int profitTwoConditional=0;
+        double profitTotal = 0; double broughtTotal = 0;
+        while(!nameThing.equalsIgnoreCase("X")) {
+            System.out.println("Digite o nome da mercadoria, X parar o sistema");
+            nameThing = t.next();
+            if(!nameThing.equalsIgnoreCase("X")) {
+                System.out.println("Digite o preço de compra da mercadoria");
+                double broughtPrice = t.nextDouble();
+                broughtTotal += broughtPrice;
+                
+                System.out.println("Digite o preço de venda da mercadoria");
+                double salePrice = t.nextDouble();
+
+                
+                double profit = salePrice - broughtPrice ;
+                profitTotal += profit;
+                double tenPercent =(broughtPrice * 10)/100;
+                double twentyPercent = (broughtPrice * 20)/100;
+                if( profit < tenPercent ) {
+                    profitTenpercent += 1;
+                } else if( tenPercent <= profit &&profit <= twentyPercent) {
+                    profitTwoConditional += 1;
+                } else {
+                    profitTwentypercent +=1;
+                }
+            
+            }
+            
+        }
+        
+        System.out.println(profitTenpercent+" Produtos tem lucro abaixo de 10%");
+        System.out.println(profitTwoConditional+" Produtos tem lucro entre 10% e 20%");
+        System.out.println(profitTwentypercent+" Produtos tem lucro acima de 20%");
+        System.out.println("Total gasto comprando produtos R$ "+d.format(broughtTotal));
+        System.out.println("Lucro R$ "+d.format(profitTotal));
+    }
 }
--- src/listawhile/Questao04.java
+++ src/listawhile/Questao04.java
@@ -5,10 +5,57 @@
  */
 package listawhile;
 
+import java.util.Scanner;
+
 /**
  *
  * @author JoãoMarcus
  */
 public class Questao04 {
-    
+    public static void main(String[] args) {
+        /*
+         o código do curso, o número de vagas, número de candidatos 
+        do sexo masculino 
+        e o número de candidatos do sexo feminino
+        */
+        Scanner t = new Scanner(System.in);
+        int codeCurse = 0, wave = 0,
+        beforeTotal = 0, bestCodeCurse = 0;
+        double  bestTotal = 0, Total =0;
+        String nameCurse = "";
+        
+        while(!nameCurse.equalsIgnoreCase("X") ) {
+            System.out.println("Digite o nome do Curso, X para sair");
+            nameCurse = t.next();
+
+            if(!nameCurse.equalsIgnoreCase("X")) {
+                System.out.println("Digite o codigo do curso");
+
+                codeCurse = t.nextInt();
+
+
+                System.out.println("Quantas vagas tem no curso?");
+                wave = t.nextInt();
+                System.out.println("Números de canditados do sexo masculino?");
+                int candidateMale = t.nextInt();
+                System.out.println("Números de canditados do sexo feminino?");
+                int candidateFemale = t.nextInt();
+
+                
+                Total = ((candidateMale + candidateFemale) / (wave));
+                
+                
+                if(Total > bestTotal) {
+                    bestCodeCurse =  codeCurse;
+                    bestTotal = Total;
+                }
+                int percentFemela = ((candidateFemale * 100) / (candidateMale + candidateFemale));
+                System.out.println("O número de candidatos por vagas do curso "+nameCurse+" é "+Total);
+                System.out.println("A porcetagem de candidatas  do sexo feminino é "+percentFemela+"%");
+                
+                System.out.println("O codigo do curso "+bestCodeCurse+" e o maior número de candidatos por vagas é "+bestTotal);
+                
+            } 
+        }
+    }
 }
